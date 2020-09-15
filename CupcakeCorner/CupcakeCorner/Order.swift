f47a517b49f72e327827aaa477a218568be60d48
Larry N <larryn35@gmail.com>
2020-09-14 23:06:27 -0700

    add order menu

---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Larry Nguyen on 9/14/20.
+//  Copyright © 2020 Larry Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+
+}
